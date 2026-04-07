8a192553a06b6fbe2e26643ad44b0362da6e00b4
Space Gaori <spacegaori@gmail.com>
2026-04-07 14:37:08 -0700

    Add: anagram-groups - submission-15

---
+++ Data Structures & Algorithms/anagram-groups/submission-15.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <string>
+#include <ranges>
+#include <algorithm>
+#include <unordered_map>
+
+class Solution {
+public:
+    std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs) {
+        std::unordered_map<std::string, std::vector<std::string>> anagram_group{};
+        for (const auto& str : strs) {
+            auto str_sorted = str;
+            std::ranges::sort(str_sorted);
+            anagram_group[str_sorted].push_back(str);
+        }
+
+        std::vector<std::vector<std::string>> output{};
+        for (const auto& [_, group] : anagram_group) {
+            output.push_back(group);
+        }
+
+        return output;
+    }
+};
