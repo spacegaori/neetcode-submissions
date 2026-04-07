cb20cdbe2e7ce26553fba1068ed4043b547cd3b1
Space Gaori <spacegaori@gmail.com>
2026-04-07 13:54:35 -0700

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,17 @@
+#include <ranges>
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::unordered_map<int, std::size_t> mp{};
+
+        for (const auto [index, num] : std::views::enumerate(nums)) {
+            if (mp.contains(target - num)) {
+                return { mp[target-num], index };
+            }
+            mp[num] = index;
+        }
+        
+        return { -1, -1 };
+    }
+};
