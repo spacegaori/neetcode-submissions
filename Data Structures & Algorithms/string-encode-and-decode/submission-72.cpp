2e7978dbc8b46c687bca773451a9c029aefe7a2d
Space Gaori <spacegaori@gmail.com>
2026-04-07 17:03:00 -0700

    Add: string-encode-and-decode - submission-72

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-72.cpp
@@ -0,0 +1,31 @@
+#include <cassert> 
+
+class Solution {
+public:
+    std::string encode(std::vector<std::string>& strs) {
+        auto str_encoded{ ""s };
+        for (auto& str : strs) {
+            auto len = static_cast<int>(str.size());
+            str_encoded += std::to_string(len) + "#" + str;
+        }
+
+        return str_encoded;
+    }
+
+    std::vector<std::string> decode(std::string s) {
+        std::vector<std::string> decoded_strs{};
+        std::size_t i = 0;
+        while (i < s.size()) {
+            auto j = i;
+            while (s[j] != '#') {
+                ++j;
+            }
+            auto len = std::stoi(s.substr(i, j - i));
+            ++j;
+            decoded_strs.push_back(s.substr(j, len));
+            i = j + len;
+        }
+
+        return decoded_strs;
+    }
+};
