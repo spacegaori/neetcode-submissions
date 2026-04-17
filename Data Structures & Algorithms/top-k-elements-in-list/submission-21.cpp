bd8947e8fcc283bd6233dae5034e521be695460a
Space Gaori <spacegaori@gmail.com>
2026-04-17 15:19:54 -0700

    Add: top-k-elements-in-list - submission-21

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-21.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    std::vector<int> topKFrequent(std::vector<int>& nums, int k) {
+        std::unordered_map<int, int> count{};
+        for (const int num : nums) {
+            ++count[num];
+        }
+
+        std::unordered_map<int, std::vector<int>> bucket{};
+        for (auto [key, value] : count) {
+            bucket[value].emplace_back(key);
+        }
+
+        std::vector<int> top{};
+        for (auto i{ std::ssize(nums) }; i >= 0; --i) {
+            if (bucket[i].empty()) {
+                continue;
+            }
+            for (int e : bucket[i]) {
+                top.emplace_back(e);
+
+                if (top.size() == k) {
+                    return top;
+                }
+            }
+        }
+
+        return top;
+    }
+};
