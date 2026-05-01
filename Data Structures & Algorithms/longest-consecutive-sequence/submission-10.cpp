dbdc89dbff2ebd4845a4ee00ecf152538cd1e6dc
Space Gaori <spacegaori@gmail.com>
2026-04-30 17:59:20 -0700

    Add: longest-consecutive-sequence - submission-10

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-10.cpp
@@ -0,0 +1,21 @@
+#include <ranges>
+
+class Solution {
+public:
+    int longestConsecutive(std::vector<int>& nums) {
+        std::unordered_set<int> head{};
+        int max_streak = 0;
+        for (int num : nums) {
+            if (auto it = std::ranges::find(head, num - 1) == head.end()) {
+                head.insert(num);
+                int streak = 1;
+                while (std::ranges::find(nums, ++num) != nums.end()) {
+                    ++streak;
+                }
+                max_streak = std::max(max_streak, streak);
+            }
+        }
+
+        return max_streak;
+    }
+};
