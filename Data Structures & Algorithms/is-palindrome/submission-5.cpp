99bca8abb19cce37671cfb2825db186bfb467a77
Space Gaori <spacegaori@gmail.com>
2026-05-01 18:55:45 -0700

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(std::string s) {
+        if (s.size() < 2) {
+            return true;
+        }
+
+        int left = 0;
+        int right = static_cast<int>(s.size() - 1);
+
+        while (left < right) {
+            while (left < right && !std::isalnum(s[left])) {
+                ++left;
+            }
+            while (left < right && !std::isalnum(s[right])) {
+                --right;
+            }
+            if (std::tolower(s[left]) != std::tolower(s[right])) {
+                return false;
+            }
+            ++left;
+            --right;
+        }
+
+        return true;
+    }
+};
