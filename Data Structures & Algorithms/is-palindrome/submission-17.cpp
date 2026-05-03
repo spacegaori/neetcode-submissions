46cbd4223f46bd2542bffaad14c5ab4bb4b2d874
Space Gaori <spacegaori@gmail.com>
2026-05-02 17:08:21 -0700

    Add: is-palindrome - submission-17

---
+++ Data Structures & Algorithms/is-palindrome/submission-17.cpp
@@ -0,0 +1,14 @@
+#include <ranges>
+
+class Solution {
+public:
+    bool isPalindrome(std::string s) {
+        auto isalnum = [](const auto c){ return std::isalnum(c); };
+        auto tolower = [](auto c){ return std::tolower(c); };
+
+        auto alnum = s | std::views::filter(isalnum)
+                       | std::views::transform(tolower);
+
+        return std::ranges::equal(alnum, alnum | std::views::reverse);
+    }
+};
