afb84928275a7b30b711d54c149549d3503000a3
Space Gaori <spacegaori@gmail.com>
2026-05-02 17:04:34 -0700

    Add: is-palindrome - submission-11

---
+++ Data Structures & Algorithms/is-palindrome/submission-11.cpp
@@ -0,0 +1,11 @@
+#include <ranges>
+
+class Solution {
+public:
+    bool isPalindrome(std::string s) {
+        auto alnum = s | std::views::filter([](auto c) { return std::isalnum(c); })
+                         | std::views::transform([](auto c) { return std::tolower(c); });
+
+        return std::ranges::equal(alnum, alnum | std::views::reverse);
+    }
+};
