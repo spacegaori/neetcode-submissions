619cb9564aadc91875e6b2837817450cf3301547
Space Gaori <spacegaori@gmail.com>
2026-05-07 16:09:01 -0700

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(std::vector<int>& prices) {
+        int n{ static_cast<int>(prices.size()) };
+
+        int max_profit{ 0 };
+        for (int bought_at = 0; bought_at < n - 1; ++bought_at) {
+            for (int sold_at = bought_at + 1; sold_at < n; ++sold_at) {
+                max_profit = std::max(max_profit, prices[sold_at] - prices[bought_at]);
+            }
+        }
+        return max_profit;
+    }
+};
