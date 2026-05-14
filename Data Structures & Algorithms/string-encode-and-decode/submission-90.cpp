f3e5af5b23f03a73b329c2a8a421a7ea834dacc0
Space <spacegaori@gmail.com>
2026-05-13 20:50:30 -0700

    Add: string-encode-and-decode - submission-90

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-90.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    std::string encode(const std::vector<std::string>& strs) {
+        std::string encoded_str = "";
+
+        for (const std::string& s : strs) {
+            encoded_str += std::to_string(s.size());
+            std::cout << std::to_string(s.size());
+            encoded_str += '@';
+            std::cout << '@';
+            encoded_str += s;
+            std::cout << s;
+        }
+
+        std::cout << encoded_str;
+        return encoded_str;
+    }
+
+    std::vector<std::string> decode(const std::string &s) {
+        int i = 0;
+        std::vector<std::string> decoded_strs;
+        while (i < s.size()) {
+            int j = i;
+            while (j < s.size() && s[j] != '@') {
+                j++;
+            }
+            // j == '@'
+            int len = std::stoi(s.substr(i, j - i + 1));
+            i = j + 1;
+
+            std::string word = s.substr(i, len);
+
+            decoded_strs.push_back(word);
+            i += len;
+        }
+
+        return decoded_strs;
+    }
+};
