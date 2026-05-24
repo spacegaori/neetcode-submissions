73040c3f8f72cd54cc7d4053534945d293083c0e
Space <spacegaori@gmail.com>
2026-05-23 18:07:13 -0700

    Add: find-minimum-in-rotated-sorted-array - submission-25

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-25.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findMin(std::vector<int> &nums) {
+        int l = 0;
+        int r = nums.size() - 1;
+
+        // find pivot
+        while (l < r) {
+            if (nums[l] < nums[r]) {
+                return nums[l];
+            }
+
+            int m = l + (r - l) / 2;
+            // middle is within upper bound 
+            if (nums[m] < nums[r]) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return nums[l];
+    }
+};
