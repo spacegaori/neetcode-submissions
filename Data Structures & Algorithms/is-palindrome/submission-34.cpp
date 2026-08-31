b38d5034d6f37ebb68c0f0ec858a14506e7ec6e1
Space <spacegaori@gmail.com>
2026-08-31 14:29:19 -0700

    Add: is-palindrome - submission-34

---
+++ Data Structures & Algorithms/is-palindrome/submission-34.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(const std::string& s) {
+        int l{ 0 };
+        int r{ static_cast<int>(s.length()) - 1 };
+
+        while (l < r) {
+            while (l < r && !std::isalnum(static_cast<unsigned char>(s[l]))) {
+                l++;
+            }
+            while (l < r && !std::isalnum(static_cast<unsigned char>(s[r]))) {
+                r--;
+            }
+
+            if (std::tolower(static_cast<unsigned char>(s[l])) != std::tolower(static_cast<unsigned char> (s[r]))) {
+                return false;
+            }
+
+            l++; r--;
+        }
+
+        return true;
+    }
+};
